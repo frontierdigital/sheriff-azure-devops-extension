f10dded7fe599f1e9b279bfeb50c5034a9e7ae31
Fraser Davidson <fraser@frontierhq.com>
2024-11-21 15:33:33 +0000

    WIP

--- tasks/InstallSheriffCLI/InstallSheriffCLIV0/src/index.js
+++ tasks/InstallSheriffCLI/InstallSheriffCLIV0/src/index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 const axios = require('axios');
+// const azdevops = require('azure-devops-node-api');
 const fs = require('fs');
 const path = require('path');
+const secureFilesCommon = require('azure-pipelines-tasks-securefiles-common/securefiles-common');
 const stream = require('stream');
 const tl = require('azure-pipelines-task-lib/task');
 const { promisify } = require('util');
@@ -11,6 +13,7 @@ const finished = promisify(stream.finished);
 
 async function run() {
   try {
+    const licenseSecureFile = tl.getInput('licenseSecureFile', false);
     const version = tl.getInput('version', true);
 
     const agentOS = tl.getVariable('Agent.OS');
@@ -75,6 +78,24 @@ async function run() {
 
     tl.mkdirP(toolDirPath);
     await tl.execAsync('tar', ['-xf', downloadPath, '-C', toolDirPath]);
+
+    // const accessToken = tl.getInput('System.AccessToken', true);
+    // const collectionUri = tl.getVariable('System.CollectionUri');
+
+    if (licenseSecureFile) {
+      // const authHandler = azdevops.getPersonalAccessTokenHandler(accessToken);
+      // const connection = new azdevops.WebApi(collectionUri, authHandler);
+
+      // const taskAgentApi = await connection.getTaskAgentApi();
+
+      // await taskAgentApi.getSecureFilesByNames()
+      const secureFileHelpers = new secureFilesCommon.SecureFileHelpers();
+      const licenseFilePath = await secureFileHelpers.downloadSecureFile(licenseSecureFile);
+
+      tl.debug(`Licence file path: ${licenseFilePath}`);
+
+      await tl.execAsync('cp -f', [licenseFilePath, toolDirPath]);
+    }
 
     // eslint-disable-next-line no-console
     console.log(`##vso[task.prependpath]${toolDirPath}`);
